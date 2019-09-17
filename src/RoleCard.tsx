46e8ac9cdc6d83ccb669d71db71f36c2f3ad163b
Lucas Connors <lucas.revolutiontech@gmail.com>
2019-09-16 23:43:08 -0700

    Add passing state to prepare for next player
    This state tells the player to pass the device to their left
    after they've seen their role

--- src/RoleCard.tsx
+++ src/RoleCard.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { Paper, Typography, LinearProgress } from "@material-ui/core";
-import PlaceholderTypography from "./PlaceholderTypography";
 import "./RoleCard.css";
 
 enum RevealState {
   WAITING = "waiting",
-  REVEALED = "revealed"
+  HOLDING = "holding",
+  REVEALED = "revealed",
+  PASSING = "passing"
 }
 
 interface Props {
@@ -26,39 +27,34 @@ export default class RoleCard extends React.PureComponent<Props, State> {
     revealProgress: 0
   };
 
-  private revealStart = () => {
-    const revealTimer =
-      this.state.revealTimer == null
-        ? window.setInterval(this.incrementRevealProgress, 500)
-        : this.state.revealTimer;
+  componentDidMount() {
     this.setState({
       revealState: RevealState.WAITING,
-      revealTimer: revealTimer,
+      revealTimer: window.setInterval(this.updateRevealProgress, 500),
       revealProgress: 0
     });
-  };
+  }
 
-  private incrementRevealProgress = () => {
-    if (this.state.revealProgress < 100) {
-      this.setState({ revealProgress: this.state.revealProgress + 50 });
-    } else if (this.state.revealTimer != null) {
-      window.clearInterval(this.state.revealTimer);
-      this.setState({
-        revealState: RevealState.REVEALED,
-        revealTimer: null
-      });
-    }
-  };
-
-  private revealEnd = () => {
+  componentWillUnmount() {
     if (this.state.revealTimer != null) {
       window.clearInterval(this.state.revealTimer);
     }
-    this.setState({
-      revealState: RevealState.WAITING,
-      revealTimer: null,
-      revealProgress: 0
-    });
+  }
+
+  private updateRevealProgress = () => {
+    if (this.state.revealState === RevealState.HOLDING) {
+      if (this.state.revealProgress < 100) {
+        this.setState({ revealProgress: this.state.revealProgress + 50 });
+      } else {
+        this.setState({ revealState: RevealState.REVEALED });
+      }
+    } else if (this.state.revealState === RevealState.PASSING) {
+      if (this.state.revealProgress > 0) {
+        this.setState({ revealProgress: this.state.revealProgress - 50 });
+      } else {
+        this.setState({ revealState: RevealState.WAITING });
+      }
+    }
   };
 
   render() {
@@ -72,20 +68,8 @@ export default class RoleCard extends React.PureComponent<Props, State> {
         className="RoleCard"
       >
         <Typography variant="h5">Hello, {this.props.playerName}.</Typography>
-        <PlaceholderTypography
-          predicate={this.state.revealState === RevealState.WAITING}
-          className="pt1"
-        >
-          When you're ready, hold down to reveal your role.
-        </PlaceholderTypography>
-
-        <PlaceholderTypography
-          predicate={this.state.revealState === RevealState.REVEALED}
-          className="pt6 pb6"
-        >
-          You are a member of the <strong>{this.props.team}</strong> team!
-        </PlaceholderTypography>
-
+        <Typography className="pt1">{this.getMainCopy()}</Typography>
+        <Typography className="pt6 pb6">{this.getRevealCopy()}</Typography>
         <LinearProgress
           variant="determinate"
           value={this.state.revealProgress}
@@ -94,4 +78,43 @@ export default class RoleCard extends React.PureComponent<Props, State> {
       </Paper>
     );
   }
+
+  private revealStart = () => {
+    if (this.state.revealState == RevealState.WAITING) {
+      this.setState({ revealState: RevealState.HOLDING });
+    }
+  };
+
+  private revealEnd = () => {
+    if (this.state.revealState === RevealState.HOLDING) {
+      this.setState({ revealState: RevealState.WAITING, revealProgress: 0 });
+    } else if (this.state.revealState === RevealState.REVEALED) {
+      this.setState({ revealState: RevealState.PASSING });
+    }
+  };
+
+  private getMainCopy = () => {
+    switch (this.state.revealState) {
+      case RevealState.WAITING:
+      case RevealState.HOLDING:
+        return <>When you're ready, hold down to reveal your role.</>;
+      case RevealState.PASSING:
+        return <>Okay. Now pass the device to the player on your left.</>;
+      default:
+        return <>&nbsp;</>;
+    }
+  };
+
+  private getRevealCopy = () => {
+    switch (this.state.revealState) {
+      case RevealState.REVEALED:
+        return (
+          <>
+            You are a member of the <strong>{this.props.team}</strong> team!
+          </>
+        );
+      default:
+        return <>&nbsp;</>;
+    }
+  };
 }
